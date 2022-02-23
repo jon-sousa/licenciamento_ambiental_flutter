bb355540b0d64bb0b75a4fa14ed44e8890f2ae01
jon-sousa <jonassleandro1@gmail.com>
2022-02-23 05:38:11 -0300

    listagem de documetos anexados

--- lib/pages/criar_solicitacao_page.dart
+++ lib/pages/criar_solicitacao_page.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:licenciamento_ambiental/controllers/solicitacao_controller.dart';
-import 'package:licenciamento_ambiental/models/solicitacao.dart';
 import 'package:licenciamento_ambiental/pages/camera_page.dart';
 import 'package:provider/src/provider.dart';
 
@@ -53,6 +51,9 @@ class _CriarSolicitacaoState extends State<CriarSolicitacao> {
                 onTap: () => _adicionarDocumento(),
               ),
             ),
+            Column(
+              children: _documentoList(),
+            ),
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () async => await _cadastrarSolicitacao(),
@@ -64,17 +65,76 @@ class _CriarSolicitacaoState extends State<CriarSolicitacao> {
     );
   }
 
+  List<Widget> _documentoList() {
+    List<Widget> d = [];
+    for (var documento in documentos) {
+      d.add(Padding(
+        padding: const EdgeInsets.only(left: 24.0),
+        child: Row(
+          children: [
+            Text(documento.keys.first),
+            GestureDetector(
+              child: const Icon(Icons.close),
+              onTap: () {
+                documentos.remove(documento);
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ));
+    }
+    return d;
+  }
+
+  _mostrarDialogSucesso() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Soliciação cadastrada com sucesso'),
+            actions: [
+              TextButton(
+                  autofocus: true,
+                  style: TextButton.styleFrom(primary: Colors.green),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'))
+            ],
+          );
+        });
+  }
+
+  _mostrarDialogErro() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Erro ao cadastrar solicitação'),
+            actions: [
+              TextButton(
+                  autofocus: true,
+                  style: TextButton.styleFrom(primary: Colors.red),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'))
+            ],
+          );
+        });
+  }
+
   _cadastrarSolicitacao() {
     var imovel = _imovelController.text;
     Map<String, dynamic> responseMap = {'imovel': imovel};
     responseMap.addAll({'files': documentos});
-    try {
-      _controller
-          .cadastrarSolicitacao(responseMap)
-          .then((_) => print('Solicitacao cadastrada'));
-    } catch (error) {
-      print(error);
-    }
+    _controller.cadastrarSolicitacao(responseMap).then((_) {
+      _mostrarDialogSucesso();
+      Navigator.of(context).pop();
+    }).catchError((_) {
+      _mostrarDialogErro();
+    });
   }
 
   _adicionarDocumento() async {
@@ -84,6 +144,8 @@ class _CriarSolicitacaoState extends State<CriarSolicitacao> {
       fullscreenDialog: true,
     ));
 
-    documentos.add(documento);
+    setState(() {
+      documentos.add(documento);
+    });
   }
 }
